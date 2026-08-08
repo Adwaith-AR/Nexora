a7c6310975d8224020dffb42d62251a22dcb7a5a
Adwaith-AR <adwaith.rakhy.office@gmail.com>
2026-08-09 00:10:07 +0530

    home complete and resposvice

--- code/app.js
+++ code/app.js
@@ -1,6 +1,9 @@
 
 
 const cards = document.querySelectorAll(".card");
+const Elements = document.querySelectorAll(".element");
+const h1s = document.querySelectorAll(".h1");
+
 const collage = document.getElementById("Collage");
 collage.style.opacity = "0";
 collage.style.transform = "translateX(240px)";
@@ -8,15 +11,29 @@ collage.style.transition = "1s";
 document.body.display = "0";
 
 const observer = new IntersectionObserver(entries => {
-
           entries.forEach(entry => {
-
                     if (entry.isIntersecting) {
-
                               entry.target.style.opacity = "1";
-
                               entry.target.style.transform = "translateY(0px)";
+                    }
 
+          });
+
+});
+const h1observer = new IntersectionObserver(entries => {
+          entries.forEach(entry => {
+                    if (entry.isIntersecting) {
+                              entry.target.style.opacity = "1";
+                              entry.target.style.transform = "translateY(0px)";
+                    }
+
+          });
+
+});
+const elementobserver = new IntersectionObserver(entries => {
+          entries.forEach(entry => {
+                    if (entry.isIntersecting) {
+                              entry.target.style.opacity = "1";
                     }
 
           });
@@ -26,34 +43,31 @@ setTimeout(() => {
           collage.style.opacity = "1";
           collage.style.transform = "translateX(0px)";
 
- },300
+}, 300
 
 )
-const observer2 = new IntersectionObserver(entries => {
 
-          entries.forEach(entry => {
-
-                    if (entry.isIntersecting) {
-
-                              entry.target.style.opacity = "1";
-
-                              entry.target.style.transform = "translateX(0px)";
-
-                    }
-
-          });
-
-});
 
 cards.forEach(card => {
 
           card.style.opacity = "0";
-
           card.style.transform = "translateY(40px)";
-
           card.style.transition = "0.6s";
-
           observer.observe(card);
+
+});
+Elements.forEach(element => {
+
+          element.style.opacity = "0";
+          element.style.transition = "1.6s";
+          elementobserver.observe(element);
+
+});
+h1s.forEach(h1 => {
+          h1.style.transform = "translateX(-40vw)";
+          h1.style.opacity = "0";
+          h1.style.transition = "1.6s";
+          h1observer.observe(h1);
 
 });
 
