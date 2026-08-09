3ba487c0aa74d0b88936815dacaa06fa0e76a1ef
Adwaith-AR <adwaith.rakhy.office@gmail.com>
2026-08-09 21:36:02 +0530

    Tools page updated

---
+++ code/Tools/app.js
@@ -0,0 +1,10 @@
+const mobilemenu = document.getElementById("mobilemenu");
+mobilemenu.style.display = "none"
+
+function openMenu() {
+          mobilemenu.style.display = "block";
+}
+
+function closeMenu() {
+          mobilemenu.style.display = "none";
+}
